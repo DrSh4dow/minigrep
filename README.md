a18b061cea7de1e3fb89d3aa7444a9f60b364171
drsh4dow <daniel.morettiv@gmail.com>
2022-04-21 00:26:07 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# minigrep
+
+Project from The Rust Book
---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use colored::Colorize;
+use minigrep::Config;
+use std::{env, process};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("{} Problem parsing arguments: {}", "[ ERROR ]".red(), err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("{} Application error: {}", "[ ERROR ]".red(), e);
+        process::exit(1);
+    };
+}
